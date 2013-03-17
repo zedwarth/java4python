40441800cf1b26b280cff6cba041f99213f50222
zedwarth <zedwarth@gmail.com>
2013-03-17 14:55:14 -0400

    removing the import statment and referencing the full name of java.util.Scanner

---
+++ TempConv.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class TempConv {
+	public static void main(String[] args) {
+		Double fahr;
+		Double cel;
+		java.util.Scanner in;
+
+		in = new java.util.Scanner(System.in);
+		System.out.println("Enter the temperature in F: ");
+		fahr = in.nextDouble();
+
+		cel = (fahr - 32) * 5.0/9.0;
+		System.out.println("The temperature in C is: " + cel);
+
+		System.exit(0);
+	}
+}
